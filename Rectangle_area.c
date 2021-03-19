80e5e302e5066578c322d3454a0d6c0665eb2a01
Piyush-0120 <65674133+Piyush-0120@users.noreply.github.com>
2021-03-19 20:10:36 +0530

    Activity1

--- Rectangle_area.c
+++ Rectangle_area.c
@@ -2,57 +2,71 @@
 #include<math.h>
 struct point
 {
-	float x;
-	float y;
+    float x;
+    float y;
 };
 typedef struct point Point;
 
-int tCases()
+struct rectangle
 {
     int n;
-    scanf("%d",&n);
-}
-Point input()
+    float r;
+    Point p[3];
+};
+typedef struct rectangle Rect;
+
+
+Point input_one_point()
 {
     Point p;
-    scanf("%f",&p.x);
-    scanf("%f",&p.y);
+    scanf("%f", &p.x);
+    scanf("%f", &p.y);
     return p;
 }
-void input_3(Point *p)
+void input_three_points(Point p[])
 {
-    for(int i=0;i<3;i++)
-        p[i]=input();
+    for (int i = 0; i < 3; i++)
+        p[i] = input_one_point();
 }
-void compute(Point *p,float *r)
+void input_tCases(Rect R[])
 {
-    float d,d1,d2,d3;
-    d1=sqrt(pow(p[1].x - p[0].x,2)+pow(p[1].y - p[0].y,2));
-    d2=sqrt(pow(p[2].x - p[0].x,2)+pow(p[2].y - p[0].y,2));
-    d3=sqrt(pow(p[1].x - p[2].x,2)+pow(p[1].y - p[2].y,2));
-    if(d1>d2 && d1>d3)
-        d=d2*d3;
-    else if(d2>d1 && d2>d3)
-        d=d1*d3;
-    else
-        d=d1*d2;
-    *r=d;
+    scanf("%d", &R[0].n);
+    for (int i = 0; i < R[0].n; i++)
+    {
+        R[i].n = R[0].n; //each rectangle has n
+        input_three_points(R[i].p);
+    }
 }
-void display(Point *p,float *r)
+void compute(Rect R[])
 {
-    printf("Area of rectangle with vertices (%0.1f,%.1f),(%.1f,%.1f),(%.1f,%.1f) is %.1f\n",p[0].x,p[0].y,p[1].x,p[1].y,p[2].x,p[2].y,*r);
+    for (int i = 0; i < R[0].n; i++)
+    {
+        float d, d1, d2, d3;
+        d1 = sqrt(pow(R[i].p[1].x - R[i].p[0].x, 2) + pow(R[i].p[1].y - R[i].p[0].y, 2));
+        d2 = sqrt(pow(R[i].p[2].x - R[i].p[0].x, 2) + pow(R[i].p[2].y - R[i].p[0].y, 2));
+        d3 = sqrt(pow(R[i].p[1].x - R[i].p[2].x, 2) + pow(R[i].p[1].y - R[i].p[2].y, 2));
+        if (d1 > d2 && d1 > d3)
+            d = d2 * d3;
+        else if (d2 > d1 && d2 > d3)
+            d = d1 * d3;
+        else
+            d = d1 * d2;
+        R[i].r = d;
+    }
+
+}
+void display(Rect R[])
+{
+    for (int i = 0; i < R[0].n; i++)
+    {
+        printf("Area of rectangle with vertices (%0.1f,%.1f),(%.1f,%.1f),(%.1f,%.1f) is %.1f\n", R[i].p[0].x, R[i].p[0].y, R[i].p[1].x, R[i].p[1].y, R[i].p[2].x, R[i].p[2].y, R[i].r);
+    }
 }
 int main()
 {
-    int n;
-    n=tCases();
-    while(n--)
-    {
-        Point p[3];
-        float r;
-        input_3(p);
-        compute(p,&r);
-        display(p,&r);
-    }
+    Rect R[100];
+    input_tCases(R);
+    compute(R);
+    display(R);
     return 0;
 }
